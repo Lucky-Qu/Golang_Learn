652c6808a1623eb834e732a2210b6bdafba8b476
LuckyQu <super.lucky.qu@gmail.com>
2024-11-11 12:51:51 +0800

    更新web内容

---
+++ Web/04_Form/Form.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("Web/04_Form/File")))
+	http.HandleFunc("/File", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			r.ParseForm()
+			fmt.Fprintln(w, r.Form)
+		}
+	})
+	http.ListenAndServe(":8080", nil)
+}
